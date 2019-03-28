3bfded009d15f0ed567283ed2eecf1e46961e300
DirtyWhite <fupengcheng@4399inc.com>
2019-03-28 13:54:22 +0800

    rename

--- demo/src/pages/home/home.ts
+++ demo/src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Scene, PerspectiveCamera, WebGLRenderer, MeshDepthMaterial, BoxGeometry, Mesh, DirectionalLight, MeshLambertMaterial, MeshPhongMaterial, Vector3, ParametricGeometry, DoubleSide, FlatShading, PlaneGeometry, PlaneBufferGeometry, ImageUtils, RepeatWrapping, MirroredRepeatWrapping } from 'three/src/Three'
 import control from "three-orbitcontrols";
 import { FLOOR_SIZE } from '@/baseConfig';
-require('./style.scss')
+require('./home.scss')
 
 export default class Home {
 
